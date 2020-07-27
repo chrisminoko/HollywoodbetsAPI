75763090aa0fd3d729de1ecf586d555c8fee4dff
Keanan Chetty <Keanank9Chetty08@gmail.com>
2020-07-27 15:57:32 +0200

    Updated BetTypeMarket Controller

--- Controllers/BettypeMarketController.cs
+++ Controllers/BettypeMarketController.cs
@@ -25,6 +25,7 @@ namespace SportAPISever.Controllers
         public IActionResult Get() 
         {
             var result= _betTypeMarket.GetBetypeMarkets();
+            _logger.LogInformation("Get Call forBettype market :");
             return Ok(result);
 
         }
@@ -35,29 +36,106 @@ namespace SportAPISever.Controllers
         public IActionResult Get(int? id)
         {
             var results = _betTypeMarket.Get(id);
+
+            try
+            {
+                if (results == null)
+                {
+                    _logger.LogError("There is no matching Bettype with the specified Id");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError($"Something went wrong inside Get Call for the Bettype market Get action: {ex.Message}");
+                return BadRequest("Invalid model object");
+            }
+
             return Ok(results);
         }
 
 
         [HttpPost]
-        public int Post([FromBody] BettyepeMarkets bettyepeMarkets)
+        public IActionResult Post([FromBody] BettyepeMarkets bettyepeMarkets)
         {
-            return _betTypeMarket.Add(bettyepeMarkets);
+            try
+            {
+                if (bettyepeMarkets == null)
+                {
+                    _logger.LogError("Bet type Market object sent from client is null.");
+                    return BadRequest("Sport type object is null");
+                }
+
+                if (!ModelState.IsValid)
+                {
+                    _logger.LogError("Bet type Market object sent from client.");
+                    return BadRequest("Invalid model object");
+                }
+                _betTypeMarket.Add(bettyepeMarkets);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside Post action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+
+          
 
         }
 
 
         [HttpPut]
-        public int Put([FromBody] BettyepeMarkets bettyepeMarkets)
+        public IActionResult Put([FromBody] BettyepeMarkets bettyepeMarkets)
         {
-            return _betTypeMarket.Update(bettyepeMarkets);
+
+            try
+            {
+                if (bettyepeMarkets == null)
+                {
+                    _logger.LogError("Bet type Market object sent from client is null.");
+                    return BadRequest("Sport type object is null");
+                }
+
+                if (!ModelState.IsValid)
+                {
+                    _logger.LogError("Bet type Market owner object sent from client.");
+                    return BadRequest("Invalid model object");
+                }
+
+                _betTypeMarket.Update(bettyepeMarkets);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside Edit action: {ex.Message}");
+                return BadRequest("Internal Server Error: " + ex);
+            }
+
         }
 
 
         [HttpDelete("{id}")]
-        public int Delete(int id)
+        public IActionResult Delete(int id)
         {
-            return _betTypeMarket.Delete(id);
+
+            try
+            {
+                var sport = _betTypeMarket.Get(id);
+                if (sport == null)
+                {
+                    _logger.LogError($"Sport with id :{id},hasn't been found in db");
+                    return NotFound();
+                }
+                _betTypeMarket.Delete(id);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError($"Something went wrong inside DeleteSportType action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
         }
     }
 }
